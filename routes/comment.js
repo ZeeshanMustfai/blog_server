ecbaae323151c9b7dafef09ce29d45612b1f8955
Zeeshan Mustfai <zeeshanmustfai22@gmail.com>
2023-04-30 18:07:49 +0500

    add auth middleware

--- routes/comment.js
+++ routes/comment.js
@@ -2,10 +2,11 @@ const {
 	add_comment,
 	get_all_comments,
 } = require('../controller/commentController')
+const { requireAuth } = require('../middleware/authMiddleware')
 
 const Router = require('express').Router()
 
-Router.post('/:id', add_comment)
-Router.get('/', get_all_comments)
+Router.post('/:id', requireAuth, add_comment)
+Router.get('/', requireAuth, get_all_comments)
 
 module.exports = Router
--- routes/post.js
+++ routes/post.js
@@ -5,12 +5,13 @@ const {
 	get_all_posts,
 	get_one_post,
 } = require('../controller/postController')
+const { requireAuth } = require('../middleware/authMiddleware')
 const Router = require('express').Router()
 
-Router.post('/', create_post)
-Router.delete('/:id', delete_post)
-Router.put('/:id', update_post)
-Router.get('/all', get_all_posts)
-Router.get('/:id', get_one_post)
+Router.post('/', requireAuth, create_post)
+Router.delete('/:id', requireAuth, delete_post)
+Router.put('/:id', requireAuth, update_post)
+Router.get('/all', requireAuth, get_all_posts)
+Router.get('/:id', requireAuth, get_one_post)
 
 module.exports = Router
